54c42dfd6a686939d7f36c49b21c4cba2978c788
SelcukOzkara <se.oezkara@gmail.com>
2023-09-18 09:50:36 +0200

    In progress: Layout Nav Graph Menu
    Done:
    Gradle settings + Manifest
    include binding

---
+++ app/src/main/java/com/example/play4free/ui/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.example.play4free.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.play4free.R
+import com.example.play4free.databinding.FragmentDetailBinding
+
+private lateinit var binding: FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
