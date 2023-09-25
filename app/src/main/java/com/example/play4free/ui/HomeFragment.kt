f9bf493d6e772f066c07ba94262707eed93cc3d5
SelcukOzkara <se.oezkara@gmail.com>
2023-09-25 10:58:55 +0200

    In progress: Layout Menu FIREBASE DIALOG
    Done:
    Gradle settings + Manifest
    include binding
    API
    DATABASE
    NAV GRAPH

--- app/src/main/java/com/example/play4free/ui/HomeFragment.kt
+++ app/src/main/java/com/example/play4free/ui/HomeFragment.kt
@@ -1,15 +1,25 @@
 package com.example.play4free.ui
 
+import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import com.example.play4free.GameViewModel
 import com.example.play4free.R
 import com.example.play4free.adapter.GameAdapter
 import com.example.play4free.databinding.FragmentHomeBinding
+import com.example.play4free.databinding.FragmentLoginBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 private lateinit var binding: FragmentHomeBinding
 
@@ -32,9 +42,38 @@ class HomeFragment : Fragment() {
         viewModel.loadGameList()
         binding.homeRV.adapter = gameAdapter
 
+        showDialog()
+
         viewModel.gameList.observe(viewLifecycleOwner){
             gameAdapter.submitList(it)
         }
+    }
+
+
+    private fun showDialog() {
+        val binding2 : FragmentLoginBinding = FragmentLoginBinding.inflate(layoutInflater)
+        val dialog = Dialog(requireContext())
+        dialog.window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        dialog.setCancelable(false)
+        dialog.setContentView(binding2.root)
+        dialog.window?.setLayout((WindowManager.LayoutParams.MATCH_PARENT), (resources.displayMetrics.heightPixels*0.8).toInt())
+//        dialog.window?.setGravity(Gravity.BOTTOM)
+        binding.homeRV.alpha = 0.2f
+
+        binding2.loginMailET.setText("Test")
+
+        binding2.loginSignUpBTN.setOnClickListener {
+            binding.homeRV.alpha = 1f
+            dialog.dismiss()
+        }
+
+       binding2.loginSignInBTN.setOnClickListener {
+           binding.homeRV.alpha = 1f
+           dialog.dismiss()
+       }
+
+
+        dialog.show()
     }
 
 }
--- app/src/main/java/com/example/play4free/ui/LoginFragment.kt
+++ app/src/main/java/com/example/play4free/ui/LoginFragment.kt
@@ -5,19 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.play4free.GameViewModel
 import com.example.play4free.R
 import com.example.play4free.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-    lateinit var binding: FragmentLoginBinding
+    val viewModel: GameViewModel by activityViewModels()
+    private lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentLoginBinding.inflate(inflater,container,false)
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.loginSignUpBTN.setOnClickListener {
+            val email = binding.loginMailET.text.toString()
+            val pw = binding.loginPwET.text.toString()
+
+            viewModel.signUp(email,pw)
+        }
+
+        binding.loginSignInBTN.setOnClickListener {
+            val email = binding.loginMailET.text.toString()
+            val pw = binding.loginPwET.text.toString()
+
+            viewModel.signIn(email,pw)
+        }
+
+        viewModel.user.observe(viewLifecycleOwner){
+            if(it != null){
+                findNavController().navigate(R.id.homeFragment)
+            }
+        }
     }
 
 
